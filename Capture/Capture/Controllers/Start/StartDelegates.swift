b3f0fe1c83ad880bbd7b9fdf8f699dfde33cc31b
allDolinin <allDolinin@gmail.com>
2022-03-01 18:10:04 +0300

    Реализовано удаление ячейки

--- Capture/Capture/Controllers/Start/StartDelegates.swift
+++ Capture/Capture/Controllers/Start/StartDelegates.swift
@@ -12,4 +12,20 @@ extension StartViewController: UITableViewDelegate, UITableViewDataSource {
         captureCell.indexCell = indexPath.row
         return captureCell
     }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .normal, title: nil) { (action, view, completion) in
+            Archive.archiveCaptures.remove(at: indexPath.row)
+            /// запись нового массива
+            Archive.saveArchive()
+            self.viewSelf.capturesTable.reloadData()
+        }
+        deleteAction.image = UIImage(named: "trash.png")
+        deleteAction.backgroundColor = .red
+        return UISwipeActionsConfiguration(actions: [deleteAction/*, muteAction*/])
+    }
 }
